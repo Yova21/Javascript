98f4e70ad23ba46d1da40017130ddcd97cf01929
Yova21 <74622754+Yova21@users.noreply.github.com>
2021-05-04 21:26:22 -0500

    segundo commit se agrega index y js

---
+++ script.js
@@ -0,0 +1,79 @@
+
+//fundamentacion de js practica 
+alert ('hola mundo');
+console.log('hola guapos');
+
+
+
+
+//sentencias 
+//alert ('hola'); alert ('mundo');
+
+
+//variables let el primero 
+let mensaje;
+mensaje = "hola mundo"
+alert(mensaje)
+
+// var puedes declarar varias variables con un nombre pero no es recomendabñe
+
+
+let c = 5, d=2, j=3; //mala practica
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es" , suma);
+
+b = 7 
+console.log(b)
+
+let nombre = "roberto";
+let nombre2= nombre;
+
+nombre2 = "carlos";
+console.log("El nombre uno es : ", nombre);
+console.log("El nombre uno es : ", nombre2);
+
+
+//nombrar variables con camel case 
+
+nombreuno = "Yoiner";
+Nombredos = "vargas";
+
+// nombrar como underscor 
+nombre_uno = "seba";
+nombre_dos = "quiqui";
+
+//NOMBRES NO COMUNES
+
+let $ = 1;
+let _ = 2;
+
+// como no puedes nombrar variables mala practica
+
+/* let 1a =3;
+let primer-nombre = 4 */
+
+"use strict" // requerir la ultima version 
+mensaje = "Hola mundo";
+
+//constantes
+
+const PI =3.141624;
+const COLOR_ROJO = "#FFFFF"
+pi_dos = pi;
+pi_dos = 7;
+
+console.log(pi);
+console.log(pi_dos);
+
+const id_Account = get_id_account (45);
+
+
+
+
+
+
+
+
